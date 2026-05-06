6b5e3a936f153169511254999b1dd5e392f4bf97
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-06 14:47:32 +0200

    Creao array di oggetti con gli articoli

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,19 @@ import { useState } from 'react'
 import BlogForm from './components/BlogForm'
 import BlogList from './components/BlogList'
 
+const initialArticles = [
+  {
+    id: crypto.randomUUID(),
+    title: 'Introduzione a React',
+    content: 'React è una libreria JavaScript per costruire interfacce utente.',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Cos’ è useState',
+    content: 'useState permette di gestire dati dinamici all’interno di un componente.',
+  },
+]
+
 function App() {
 
 }
